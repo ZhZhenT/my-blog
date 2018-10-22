440a71b5af83f164c7528020c8b798cb2df3732c
ZhanZhenT <849723358@qq.com>
2018-10-22 17:01:17 +0800

    eslint

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,45 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+
+    <div class="post-content" v-hljs="msg">
+      <pre><code>{{msg}}</code></pre>
     </div>
-    <router-view/>
+
+    <button @click="bindclickupdatemsg">修改</button>
+
   </div>
 </template>
+<script>
 
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+const msg = `  #app {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+  }`
+
+export default {
+  data () {
+    return {
+      msg: msg
     }
+  },
+  computed: {
+
+  },
+  methods: {
+
+    bindclickupdatemsg () {
+      this.msg = `#app`
+    }
+
+  },
+  mounted () {
+
   }
 }
+</script>
+<style lang="less">
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,31 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/agate.css'
 
 Vue.config.productionTip = false
+
+/*
+Vue.directive('hljs', el => {
+let blocks = el.querySelectorAll('code');
+console.log(el,blocks)
+Array.prototype.forEach.call(blocks, hljs.highlightBlock);
+});
+*/
+
+Vue.directive('hljs', {
+  inserted: function (el) {
+    let blocks = el.querySelectorAll('code')
+    console.log(blocks, 1)
+    Array.prototype.forEach.call(blocks, hljs.highlightBlock)
+  },
+  componentUpdated: function (el, obj) {
+    let blocks = el.querySelectorAll('code')
+    blocks[0].innerHTML = obj.value
+    Array.prototype.forEach.call(blocks, hljs.highlightBlock)
+  }
+})
 
 new Vue({
   router,
